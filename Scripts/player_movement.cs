93d93495cec5db1ded89a8913d6f6f5339b0144f
Chance Yan <cyan4220@gmail.com>
2025-05-05 22:02:41 -0400

    adding project f
    version 1.0

---
+++ Scripts/player_movement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player_movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+    private Vector2 _smoothedMovementInput;
+    private Vector2 _movementInputSmoothVelocity;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _smoothedMovementInput = Vector2.SmoothDamp(
+            _smoothedMovementInput,
+            _movementInput,
+            ref _movementInputSmoothVelocity,
+            0.1f);
+
+        _rigidbody.velocity = _smoothedMovementInput * _speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();
+    }
+}
